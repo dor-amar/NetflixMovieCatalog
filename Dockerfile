c3f5ae34f0cfea8bfeedb83526a943d781ed4f6f
Dor Amar <doramar5555@gmail.com>
2024-07-24 19:19:51 +0700

    Adding Dockerfile + cicd and deploy script

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:alpine3.18
+
+WORKDIR /app
+
+COPY requirements.txt ./
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8080
+
+CMD ["python3", "app.py"]
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-# TODO your deploy script implementation...
+# Variables
+CONTAINER_NAME="netflix-movie-catalog"
+IMAGE_NAME="doramar97/netflix-movie-catalog:latest"
+
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+docker pull $IMAGE_NAME
+
+# Run the new container
+docker run \
+  -d \
+  -p 8080:8080 \
+  --network netflix \
+  --name netflix-movie-catalog \
+  doramar97/netflix-movie-catalog:latest
